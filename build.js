6ab4e01efb74becb220b2a23baa262cbcc82133b
Tom Genoni <tom@genoni.studio>
2024-08-09 15:38:48 -0700

    Light and dark working

--- build.js
+++ build.js
@@ -1,1 +1,28 @@
 import StyleDictionary from 'style-dictionary';
+
+function modeConfigs(theme) {
+  return {
+    source: [`tokens/mode/${theme}.json`],
+    include: ['tokens/core.json'],
+    platforms: {
+      css: {
+        prefix: 'pdl',
+        transformGroup: 'css',
+        files: [
+          {
+            destination: `dist/${theme}.css`,
+            format: 'css/variables',
+            filter: async (token) => {
+              return token.filePath.includes(theme);
+            },
+          },
+        ],
+      },
+    },
+  };
+}
+
+['light', 'dark'].map(function (theme) {
+  const sd = new StyleDictionary(modeConfigs(theme));
+  sd.buildPlatform('css');
+});
